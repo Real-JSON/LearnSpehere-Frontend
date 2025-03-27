f99c21a7efcf8b3ac270d6d919bc6ff1badeaf0c
JanudaStack <jaariyasinghe2003@gmail.com>
2025-03-27 19:16:11 +0530

    Add Tutor page and styles; enhance responsiveness and layout for About and Course pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/App.css"
 import About from './pages/About'
 import Benefit from './pages/Benefit'
 import Course from './pages/Course'
+import Tutor from './pages/Tutor'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <About />
       <Benefit />
       <Course />
+      <Tutor />
     </>
 
   )
