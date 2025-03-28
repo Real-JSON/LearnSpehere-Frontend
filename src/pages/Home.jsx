da4a926112d8c3d8053122c9bc5f14f926b9ace8
JanudaStack <jaariyasinghe2003@gmail.com>
2025-03-29 04:18:35 +0530

    Remove unused navigation code from Home component for cleaner structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,44 +8,6 @@ import homeImg from '../assets/3837.jpg';
 const Home = () => {
     return (
         <>
-            <header>
-                <nav>
-                    <div className="nav-links">
-                        <div className="nav-logo">
-                            <a href="">{/* <img src={uniLogo} alt="" /> */}</a>
-                        </div>
-                        <ul className="">
-                            <li className="hideOnMobile">
-                                <a href="#">Home</a>
-                            </li>
-                            <li className="hideOnMobile">
-                                <a href="#">About</a>
-                            </li>
-                            <li className="hideOnMobile">
-                                <a href="#">Students</a>
-                            </li>
-                            <li className="hideOnMobile">
-                                <a href="#">Learning</a>
-                            </li>
-                            <li className="hideOnMobile">
-                                <a href="#">Examination</a>
-                            </li>
-                            <li className="hideOnMobile">
-                                <a href="#">Short Courses</a>
-                            </li>
-                        </ul>
-                        <ul id="sign">
-                            <li>
-                                <a href="">Sign Up</a>
-                            </li>
-                            <li>
-                                <a href="">Sign In</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
-
             <section>
                 <div className="front-main-banner">
                     <picture className="front-main-banner-picture">
