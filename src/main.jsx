1b9d8dc1a17ab1a38801e1a05619cfd68fc8619a
JanudaStack <jaariyasinghe2003@gmail.com>
2025-03-31 20:32:16 +0530

    Refactor Vercel configuration and update routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
         path: '/about',
         element: <About />,
     },
+    // {
+    //     path: '*',
+    //     element: <App />,
+    // },
 ]);
 
 createRoot(document.getElementById('root')).render(
