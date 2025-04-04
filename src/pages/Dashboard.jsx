ead75332f97a59d49e591fe0c88dea11717e129f
JanudaStack <jaariyasinghe2003@gmail.com>
2025-04-04 23:57:09 +0530

    Add navigation links to Dashboard header and enhance sidebar with Payment History

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import '../styles/Dashboard.css';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <>
             <div className="grid-container">
-                <header className="dashboard-header"></header>
+                <header className="dashboard-header">
+                    <div className="nav-list">
+                        <a href="#home" className="logo">
+                            <i className="ri-donut-chart-fill"></i>
+                            <span>LearnSphere</span>
+                        </a>
+                        {/* Add the 'open' class dynamically based on state */}
+                        <ul className="nav-items">
+                            <li>
+                                <Link to="/" className="active">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <a href="#about">About</a>
+                            </li>
+                            <li>
+                                <a href="#courses">Courses</a>
+                            </li>
+                            <li>
+                                <a href="#tutor">Become a Tutor</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </header>
                 <section className="dashboard-sidebar">
                     <div className="sidebar-items-container">
                         <div className="sidebar-items">
                             <div className="s-item">Dashboard</div>
                             <div className="s-item">Subjects</div>
                             <div className="s-item">Payments</div>
+                            <div className="s-item">Payment History</div>
                         </div>
                     </div>
                 </section>
