4db66de10b659d53eab80b9f1c66863591f60d6e
JanudaStack <jaariyasinghe2003@gmail.com>
2025-04-04 23:41:50 +0530

    Add sidebar items to Dashboard component and style sidebar container

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,15 @@ const Dashboard = () => {
         <>
             <div className="grid-container">
                 <header className="dashboard-header"></header>
-                <section className="dashboard-sidebar"></section>
+                <section className="dashboard-sidebar">
+                    <div className="sidebar-items-container">
+                        <div className="sidebar-items">
+                            <div className="s-item">Dashboard</div>
+                            <div className="s-item">Subjects</div>
+                            <div className="s-item">Payments</div>
+                        </div>
+                    </div>
+                </section>
                 <main className="dashboard-main"></main>
             </div>
         </>
