31b877ef74b7383debf083819878cca7def8205f
Real-JSON <jaariyasinghe2003@gmail.com>
2026-07-21 11:06:29 +0530

    refactor: fix hero images path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ import '../styles/Home.css';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar Component/Navbar';
 
+import hero200 from '/src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_200.jpg';
+import hero863 from '/src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_863.jpg';
+import hero1290 from '/src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_1290.jpg';
+import hero1400 from '/src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_1400.jpg';
+
 const Home = () => {
     return (
         <>
@@ -10,12 +15,9 @@ const Home = () => {
             <section>
                 <div className="front-main-banner">
                     <picture className="front-main-banner-picture">
-                        <img sizes="(max-width: 1400px) 100vw, 1400px"srcSet="
-                            /src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_200.jpg 200w,
-                            /src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_863.jpg 863w,
-                            /src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_1290.jpg 1290w,
-                            /src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_1400.jpg 1400w"
-                            src="/src/assets/hero-images/freepik__upload__60907_nwpgqt_c_scale,w_1400.jpg"
+                        <img sizes="(max-width: 1400px) 100vw, 1400px"
+                            srcSet={`${hero200} 200w, ${hero863} 863w, ${hero1290} 1290w, ${hero1400} 1400w`}
+                            src={hero1400}
                             alt="hero-image"/>
                     </picture>
                     <div className="front-banner-text">
